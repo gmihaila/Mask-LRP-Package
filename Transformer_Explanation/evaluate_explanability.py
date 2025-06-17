34ae2b6529fd05f673822108bf951d1afb257289
George Mihaila <georgemihaila@my.unt.edu>
2025-06-17 07:10:15 -0500

    package updates

--- Transformer_Explanation/evaluate_explanability.py
+++ Transformer_Explanation/evaluate_explanability.py
@@ -1,6 +1,6 @@
 from Transformer_Explanation.ExplanationGenerator import Generator
 from Transformer_Explanation.utils.metrices import replace_words, cal_aopc, cal_logodds, cal_count, perfect_count
-from self_parser import parser
+from Transformer_Explanation.self_parser import parser
 from Transformer_Explanation.utils.preprocess import preprocess_sample, load_dataset_json, load_model_and_tokenizer, generate_head_mask
 import torch
 import numpy as np
